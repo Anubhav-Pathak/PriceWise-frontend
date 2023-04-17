18a41e8d5cebb93a7c9d5a77bbaa967480a021a0
Seneen Khan <seneensakinakhan@Seneens-MacBook-Air.local>
2023-04-18 00:24:43 +0530

    User form

--- src/components/Landing/UserForm.jsx
+++ src/components/Landing/UserForm.jsx
@@ -4,10 +4,34 @@ const UserForm = () => {
   return (
     <form>
     <div>
-  <label>
-    Year of manufacturing
-    <input type="number" name="year of manufacturing" />
+    <label>
+    Owner Name:
   </label>
+  <input type ="text" name = "Owner Name"/>
+ </div>
+ <div>
+  <label>
+    Car Model Name:
+  </label>
+  <input type = "text" name = "Car Model Name "/>
+ </div>
+ <div>
+ <label>
+  Car vehicle Number:
+ </label>
+ <input type = "number" name = "Car Vehicle Number"/>
+ </div>
+    <div>
+  <label>
+    Year of manufacturing:
+  </label>
+  <input type="number" name="year of manufacturing" />
+  </div>
+  <div>
+    <label>
+      Ex showroom price:
+    </label>
+    <input type = "number" name = "Ex showroom price"/>
   </div>
   <input type="submit" value="Submit" />
 </form>
