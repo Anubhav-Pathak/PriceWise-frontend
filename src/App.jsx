4f3b6e488eae949e9d470d32252f887eae4e0e7b
Aryan Raj <aryanraj2713@gmail.com>
2023-04-17 19:27:08 +0530

    bug:fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import './App.css';
 import React from 'react';
-import ImageUploader from './ui/Button'; 
 import Appbar from './components/Landing/appbar';
-import hero from './components/Landing/hero';
+
 
 function App() {
 
