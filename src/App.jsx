8c1e7f102608db9bd8a435b121d938fb8c4df02e
Seneen Khan <seneensakinakhan@Seneens-MacBook-Air.local>
2023-04-17 20:35:59 +0530

    DragDrop feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
-import React from 'react';
-import ImageUploader from './ui/Button'; 
+import React from 'react'; 
+import DragDrop from './components/Landing/DragDrop';
+
 
 function App() {
 
   return (
     <div className="App">
       <h1 className='text-4xl'>Price-Wise FrontEnd Template</h1>
+      <DragDrop/>
+    
+
     </div>
   )
 }
