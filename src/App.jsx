8fea753e6748456ef1d6be995b11ff330d038093
Aryan Raj <aryanraj2713@gmail.com>
2023-04-17 22:25:49 +0530

    Added Upload Option

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Appbar from './components/Landing/appbar';
 import Footer from './components/Landing/f';
+import Uploader from './components/Landing/Uploader';
 function App() {
 
   return (
@@ -12,7 +13,9 @@ function App() {
           <h1 className='text-4xl text-green-300'>Price-Wise FrontEnd Template</h1>
         </div>
       </main>
+      <Uploader/>
       <Footer/>
+      
     </>
   )
 }
